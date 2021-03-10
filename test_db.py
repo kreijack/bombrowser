65927a089cf437fedc6f0c3faaf1f1c33aa33977
Goffredo Baroncelli <kreijack@inwind.it>
2021-03-10 20:43:48 +0100

    Update the test to the db v0.3

--- test_db.py
+++ test_db.py
@@ -1,21 +1,32 @@
 import db
 import sys
 import datetime
+import pprint
 
 def _test_insert_items(c):
     codes = [('code123', "descr456", 0), ('code124', "descr457", 0),
             ('code135', "descr468", 0), ('code136', "descr469", 0),
             ('code123', "descr456", 1)]
+    map_code_id = {}
     for (code, descr, ver) in codes:
 
-        c.execute("""INSERT INTO items(
-            descr, code, ver,
+
+        if code in map_code_id:
+            code_id = map_code_id[code]
+        else:
+            c.execute("""INSERT INTO items(code) VALUES (?)""",( code,))
+            c.execute("""SELECT MAX (id) FROM items""")
+            code_id = c.fetchone()[0]
+            map_code_id[code] = code_id
+
+        c.execute("""INSERT INTO item_revisions(
+            descr, code_id, ver,
             iter, default_unit,
-            for1cod, for1name) VALUES (
+            gval1, gval2) VALUES (
             ?, ?, ?,
             ?, ?,
             ?, ?)""", (
-                descr, code, "%d"%(ver),
+                descr, code_id, "%d"%(ver),
                 ver, "NR",
                 "FOR1COD", "FOR1NAME"
             ))
@@ -37,7 +48,7 @@ def test_get_code():
 
     _test_insert_items(c)
 
-    data = d.get_code(1)
+    data = d.get_code(1, 0)
     assert(data["code"] == "code123")
     assert(data["descr"] == "descr456")
 
@@ -48,13 +59,30 @@ def test_get_code():
 
 
     data = d.get_codes_by_like_code("code12%")
-    assert(len(data) == 3)
+    assert(len(data) == 2)
     data.sort(key = lambda x: x[1])
     assert(data[0][1] == "code123")
     assert(data[0][2] == "descr456")
 
-    assert(data[2][1] == "code124")
-    assert(data[2][2] == "descr457")
+    assert(data[1][1] == "code124")
+    assert(data[1][2] == "descr457")
+
+    data = d.get_codes_by_like_code_and_descr("code12%", "%7")
+    assert(len(data) == 1)
+    data.sort(key = lambda x: x[1])
+    assert(data[0][1] == "code124")
+    assert(data[0][2] == "descr457")
+
+
+def test_get_code_by_rid():
+    d, c = _create_db()
+
+    _test_insert_items(c)
+
+    data = d.get_code_from_rid(1)
+    assert(data["code"] == "code123")
+    assert(data["descr"] == "descr456")
+
 
 def test_get_code_by_descr():
     d, c = _create_db()
@@ -97,115 +125,106 @@ def _test_insert_assembly(c):
 
     """
     ass = {
-        "O": [ ("A", "2020-01-10", "2020-01-14"),
-               ("A", "2020-01-15", "2020-01-19"),
-               ("A", "2020-01-20", "2020-01-24"),
-               ("A", "2020-01-25", ""), ],
+        "O": ( ("2020-01-10", "",           ("A",)), ),
 
-        "A": [ ("B", "2020-01-10", "2020-01-14"),
-               ("B", "2020-01-15", "2020-01-19"),
-               ("B", "2020-01-20", ""),
+        "A": ( ("2020-01-10", "2020-01-24", ("B", "C")),
+               ("2020-01-25", "",           ("B", "M")), ),
 
-               ("C", "2020-01-10", "2020-01-14"),
-               ("C", "2020-01-15", "2020-01-19"),
-               ("C", "2020-01-20", "2020-01-24"),
+        "B": ( ("2020-01-10", "2020-01-14", ("D", "E")),
+               ("2020-01-15", "2020-01-19", ("H", "E")),
+               ("2020-01-20", "",           ("D", "C")), ),
 
-               ("M", "2020-01-25", ""), ],
+        "C": ( ("2020-01-10", "2020-01-14", ("F", "G")),
+               ("2020-01-15", "2020-01-19", ("I", "G")),
+               ("2020-01-20", "",           ("I", "L")), ),
 
-        "B": [ ("D", "2020-01-10", "2020-01-14"),
-               ("D", "2020-01-20", ""),
+        "D": ( ("2020-01-10", "",           ()), ),
 
-               ("H", "2020-01-15", "2020-01-19"),
+        "E": ( ("2020-01-10", "",           ()), ),
 
-               ("E", "2020-01-10", ""), ],
+        "F": ( ("2020-01-10", "",           ()), ),
 
-        "C": [ ("F", "2020-01-10", "2020-01-14"),
+        "G": ( ("2020-01-10", "",           ()), ),
 
-               ("G", "2020-01-10", "2020-01-19"),
+        "H": ( ("2020-01-15", "",           ()), ),
 
-               ("I", "2020-01-15", ""),
+        "I": ( ("2020-01-15", "",           ()), ),
 
-               ("L", "2020-01-20", ""), ],
+        "L": ( ("2020-01-20", "",           ()), ),
 
-        "M": [ ("I", "2020-01-25", ""),
-
-               ("L", "2020-01-25", ""), ],
-
+        "M": ( ("2020-01-25", "",           ("I", "L")), ),
     }
 
 
-    items = set(ass.keys())
-    items2=list(items)
-    for k in items2:
-        for i,from_, to in ass[k]:
-            items.add(i)
-
     map_code_id = {}
-    for i in items:
-        c.execute("""INSERT INTO items(
-            descr, code, ver,
-            iter, default_unit,
-            for1cod, for1name) VALUES (
-            ?, ?, ?,
-            ?, ?,
-            ?, ?)""", (
-                "Items '%s'"%(i), i, "1",
-                1, "NR",
-                "FOR1COD", "FOR1NAME"
-        ))
-        c.execute("""SELECT MAX(id) FROM items""")
-        id_ = c.fetchone()[0]
-        map_code_id[i] = id_
+    iter_ = dict()
+    for code in ass:
 
-    # insert the assy by date order
+        c.execute("""INSERT INTO items(code) VALUES (?)""", (code,))
+        c.execute("""SELECT MAX (id) FROM items""")
+        code_id = c.fetchone()[0]
 
-    dates = []
-    for parent in ass:
-        for child, from_, to_ in ass[parent]:
-            if not from_ in dates:
-                dates.append(from_)
+        map_code_id[code] = code_id
+        iter_[code] = 0
 
-    dates.sort()
+     # insert the assy by date order
 
-    while len(dates):
-        the_date = dates[0]
-        dates = dates[1:]
+    for code in ass:
+        code_id = map_code_id[code]
 
-        for parent in ass:
-            for child, from_, to_ in ass[parent]:
-                if from_ != the_date:
-                    continue
 
-                if to_ == "":
-                    ts = db.end_of_the_world
-                else:
-                    ts = datetime.date.fromisoformat(to_).toordinal()
-                #print("map_code_id[child]=", map_code_id[child])
+        for (date_from, date_to, children) in ass[code]:
+
+            if date_to == "":
+                dts = db.end_of_the_world
+            else:
+                dts = db.iso_to_days(date_to)
+
+            c.execute("""INSERT INTO item_revisions(
+                descr, code_id, ver,
+                iter, default_unit,
+                date_from_days, date_from,
+                date_to_days, date_to,
+                gval1, gval2) VALUES (
+                ?, ?, ?,
+                ?, ?,
+
+                ?, ?,
+                ?, ?,
+
+                ?, ?)""", (
+                    "Items '%s'"%(i), code_id, "1",
+                    iter_[code], "NR",
+
+                    db.iso_to_days(date_from), date_from,
+                    dts, date_to,
+
+                    "FOR1COD", "FOR1NAME"
+            ))
+            iter_[code] += 1
+
+            for child in children:
+                c.execute("""SELECT MAX (id) FROM item_revisions""")
+                rid = c.fetchone()[0]
                 c.execute("""INSERT INTO assemblies (
-                    unit,
-                    child_id, parent_id,
-                    qty,
-                    each,
-                    date_from, date_to,
-                    date_from_days,
-                    date_to_days,
-                    iter, ref) VALUES (
-                    ?,
-                    ?, ?,
-                    ?,
-                    ?,
-                    ?, ?,
-                    ?, ?,
-                    ?, ? )""", (
-                        "NR",
-                        map_code_id[child], map_code_id[parent],
-                        1,
-                        1,
-                        from_, to_,
-                        datetime.date.fromisoformat(from_).toordinal(),
-                        ts,
-                        0, '//')
-                )
+                        unit,
+                        child_id, revision_id,
+                        qty,
+                        each,
+                        ref
+                    ) VALUES (
+                        ?,
+                        ?, ?,
+                        ?,
+                        ?,
+                        ?
+                    )""", (
+                            "NR",
+                            map_code_id[child], rid,
+                            1,
+                            1,
+                            '//')
+                    )
 
 def test_is_assembly():
     d, c = _create_db()
@@ -222,10 +241,10 @@ def test_get_dates():
 
     id_ = d.get_codes_by_code("A")[0][0]
 
-    dates = [ x[1] for x in d.get_dates_by_code_id(id_)]
-    assert(len(dates) == 4)
+    dates = [ x[2] for x in d.get_dates_by_code_id2(id_)]
+    assert(len(dates) == 2)
     assert("2020-01-10" in dates)
-    assert("2020-01-20" in dates)
+    assert("2020-01-25" in dates)
 
 def test_get_bom():
     d, c = _create_db()
@@ -253,6 +272,68 @@ def test_get_bom():
     assert(find_in_bom("H"))
     assert(find_in_bom("O"))
 
+def test_get_bom_dates_by_code_id():
+    d, c = _create_db()
+
+    _test_insert_assembly(c)
+
+    id_ = d.get_codes_by_code("O")[0][0]
+
+    dates = d.get_bom_dates_by_code_id(id_)
+    assert(len(dates) == 4)
+
+    assert("2020-01-10" in [x[0] for x in dates])
+    assert("2020-01-25" in [x[0] for x in dates])
+
+    id_ = d.get_codes_by_code("H")[0][0]
+
+    dates = d.get_bom_dates_by_code_id(id_)
+
+    assert(len(dates) == 1)
+
+    assert("2020-01-15" in [x[0] for x in dates])
+
+    id_ = d.get_codes_by_code("B")[0][0]
+
+    dates = d.get_bom_dates_by_code_id(id_)
+
+    assert(len(dates) == 3)
+
+    assert("2020-01-10" in [x[0] for x in dates])
+    assert(not "2020-01-25" in [x[0] for x in dates])
+
+def test_get_children_by_rid():
+    d, c = _create_db()
+
+    _test_insert_assembly(c)
+
+    id_ = d.get_codes_by_code("A")[0][0]
+    rid = d.get_code(id_, db.iso_to_days("2020-01-10"))["rid"]
+    children = d.get_children_by_rid(rid)
+    assert(len(children) == 2)
+
+    assert("B" in [x[1] for x in children])
+    assert(not "M" in [x[1] for x in children])
+
+    id_ = d.get_codes_by_code("E")[0][0]
+    rid = d.get_code(id_, db.iso_to_days("2020-01-10"))["rid"]
+    children = d.get_children_by_rid(rid)
+    assert(len(children) == 0)
+
+    id_ = d.get_codes_by_code("C")[0][0]
+    rid = d.get_code(id_, db.iso_to_days("2020-01-10"))["rid"]
+    children = d.get_children_by_rid(rid)
+    assert(len(children) == 2)
+    assert("F" in [x[1] for x in children])
+    assert("G" in [x[1] for x in children])
+
+    id_ = d.get_codes_by_code("C")[0][0]
+    rid = d.get_code(id_, db.iso_to_days("2020-01-20"))["rid"]
+    children = d.get_children_by_rid(rid)
+    assert(len(children) == 2)
+    assert("I" in [x[1] for x in children])
+    assert("L" in [x[1] for x in children])
+
 def test_where_used():
     d, c = _create_db()
 
@@ -278,6 +359,34 @@ def test_where_used():
     assert(not find_in_bom("L"))
     assert(find_in_bom("H"))
     assert(find_in_bom("O"))
+
+def test_valid_where_used():
+    d, c = _create_db()
+
+    _test_insert_assembly(c)
+
+    def find_in_bom(code):
+        for k in bom:
+            if code == bom[k]["code"]:
+                return True
+        else:
+            return False
+
+    id_ = d.get_codes_by_code("D")[0][0]
+    (root, bom) = d.get_where_used_from_id_code(id_)
+    assert(len(bom) == 6) # 5 fathers,grand... + the root node = 6
+
+    assert(find_in_bom("A"))
+    assert(not find_in_bom("H"))
+    assert(find_in_bom("O"))
+
+    id_ = d.get_codes_by_code("H")[0][0]
+    (root, bom) = d.get_where_used_from_id_code(id_)
+
+    assert(not find_in_bom("L"))
+    assert(find_in_bom("H"))
+    assert(find_in_bom("O"))
+
 
 def run_test(filters):
     import inspect
