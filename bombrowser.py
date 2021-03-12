e6af5f97ccc0e7b7c897f59146787215551dcbee
Goffredo Baroncelli <kreijack@inwind.it>
2021-03-12 22:03:15 +0100

    Add command line to open window

--- bombrowser.py
+++ bombrowser.py
@@ -15,6 +15,36 @@ def main(args):
     app = QApplication(sys.argv)
     w = listcodegui.CodesWindow()
     w.show()
+
+    i = 0
+    while i < len(args):
+        if args[i] == "--whereused":
+            import asmgui
+            i += 1
+            asmgui.where_used(int(args[i]), None)
+        elif args[i] == "--validwhereused":
+            import asmgui
+            i += 1
+            asmgui.valid_where_used(int(args[i]), None)
+        elif args[i] == "--showassembly":
+            import asmgui
+            i += 1
+            asmgui.show_assembly(int(args[i]), None)
+        elif args[i] == "--showlatestassembly":
+            import asmgui
+            i += 1
+            asmgui.show_latest_assembly(int(args[i]))
+        elif args[i] == "--editcode":
+            import editcode
+            i += 1
+            editcode.edit_code_by_code_id(int(args[i]))
+
+        else:
+            print("Ignore command '%s'"%(args[i]))
+
+        i += 1
+
+
     sys.exit(app.exec_())
-    
+
 main(sys.argv)
