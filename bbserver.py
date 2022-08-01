427f0118e9dc281e2247b29284e1d7965fa96d01
Goffredo Baroncelli <kreijack@inwind.it>
2022-08-01 20:36:11 +0200

    bbserver: initial self test

--- bbserver.py
+++ bbserver.py
@@ -77,7 +77,7 @@ class RemoteSQLClient:
             data += d
 
         ret, excp = pickle.loads(data)
-        if excp:
+        if not excp is None:
             raise Exception(excp)
         if func_name == "remote_server_do_auth" and ret:
             self._username = args[0]
@@ -142,7 +142,7 @@ class _ServerHandler(socketserver.BaseRequestHandler):
                     ret = getattr(remote_db_instance, name)(*args, **kwargs)
                     excp = None
                 except Exception as e:
-                    excp = e
+                    excp = str(e) # sometime you cannot serialize an exception
                     ret = None
 
             data = pickle.dumps((ret, excp))
@@ -195,6 +195,7 @@ class RemoteSQLServer:
             "revise_code",
             "copy_code",
             "create_db",
+            "create_first_code",
         ]
         self._db = db
         self._allow_access = False
@@ -205,7 +206,7 @@ class RemoteSQLServer:
         self._read_only = False       
         return True
 
-    def __getattr__(self, name):
+    def _getattr(self, name):
         if not self._allow_access:
             raise Exception("You have to authenticate")
 
@@ -217,10 +218,20 @@ class RemoteSQLServer:
                 raise Exception("Unknown method '%s'"%(name))
 
         if not callable(getattr(self._db, name)):
-                raise Exception("Method '%s' is not callable"%(name))
+            raise Exception("Method '%s' is not callable"%(name))
 
         return getattr(self._db, name)
 
+    def __getattr__(self, name):
+        try:
+            return self._getattr(name)
+        except Exception as e:
+            class Caller:
+                def __init__(self, e):
+                    self._e = e
+                def __call__(self, *x):
+                    raise self._e
+            return Caller(e)
 
 def _start_server(db_instance, addr='0.0.0.0', port=8765, verbose=False):
     class Server_(socketserver.ThreadingTCPServer):
@@ -243,8 +254,204 @@ def _start_server(db_instance, addr='0.0.0.0', port=8765, verbose=False):
         # interrupt the program with Ctrl-C
         server.serve_forever()
 
+def _test_get_conn():
+    r = RemoteSQLClient()
+    r.remote_server_do_auth("foo", "bar")
+    return r
+
+def test_000_init_db():
+    r = _test_get_conn()
+    r.create_db()
+
+def test_010_create_first_code():
+    r = _test_get_conn()
+    r.create_db()
+    r.create_first_code()
+
+def test_020_get_codes_by_like_code():
+    r = _test_get_conn()
+    r.create_db()
+    r.create_first_code()
+
+    res = r.get_codes_by_like_code('%')
+    assert(len(res) == 1)
+
+def test_030_get_codes_by_code():
+    r = _test_get_conn()
+    r.create_db()
+    r.create_first_code()
+
+    res = r.get_codes_by_like_code('%')
+    code = res[0][1]
+    
+    res = r.get_codes_by_code(code)
+    assert(len(res) == 1)
+
+def test_040_get_dates_by_code_id3():
+    r = _test_get_conn()
+    r.create_db()
+    r.create_first_code()
+
+    res = r.get_codes_by_like_code('%')
+    code_id = res[0][0]
+    res = r.get_dates_by_code_id3(code_id)
+    assert(len(res) == 1)
+    
+def test_050_copy_code():
+    r = _test_get_conn()
+    r.create_db()
+    r.create_first_code()
+
+    res = r.get_codes_by_like_code('%')
+    code_id = res[0][0]
+    res = r.get_dates_by_code_id3(code_id)
+    rid = res[0][4]
+    
+    r.copy_code('1', rid, "new-descr", 0)
+
+    res = r.get_codes_by_like_code('%')
+    assert(len(res) == 2)
+
+
+def test_050_delete_code():
+    r = _test_get_conn()
+    r.create_db()
+    r.create_first_code()
+
+    res = r.get_codes_by_like_code('%')
+    code_id = res[0][0]
+    res = r.get_dates_by_code_id3(code_id)
+    rid = res[0][4]
+    
+    r.copy_code('1', rid, "new-descr", 0)
+
+    code_id = None
+    res = r.get_codes_by_like_code('%')
+    for line in res:
+        if line[1].startswith("0000"):
+            code_id = line[0]
+            break
+
+    res = r.delete_code(code_id)
+
+    res = r.get_codes_by_like_code('%')
+    assert(len(res) == 1)
+
+
+def test_050_update_by_rid2():
+    r = _test_get_conn()
+    r.create_db()
+    r.create_first_code()
+
+    res = r.get_codes_by_like_code('%')
+    code_id = res[0][0]
+    res = r.get_dates_by_code_id3(code_id)
+    rid = res[0][4]
+
+    r.update_by_rid2(rid, "new-descr", 0, "NR", 
+        ['x' for x in range(db.gvals_count)])
+    
+    res = r.get_codes_by_like_code('%')
+    assert(res[0][2] == "new-descr")
+
+def test_050_revise_code():
+    r = _test_get_conn()
+    r.create_db()
+    r.create_first_code()
+
+    res = r.get_codes_by_like_code('%')
+    code_id = res[0][0]
+    res = r.get_dates_by_code_id3(code_id)
+    assert(len(res) == 1)
+    rid = res[0][4]
+
+    r.revise_code(rid, "new-descr", 'A')
+    res = r.get_dates_by_code_id3(code_id)
+    assert(len(res) == 2)
+
+def test_060_delete_code_revision():
+    r = _test_get_conn()
+    r.create_db()
+    r.create_first_code()
+
+    res = r.get_codes_by_like_code('%')
+    code_id = res[0][0]
+    res = r.get_dates_by_code_id3(code_id)
+    assert(len(res) == 1)
+    rid = res[0][4]
+
+    r.revise_code(rid, "new-descr", 'A')
+    res = r.get_dates_by_code_id3(code_id)
+    assert(len(res) == 2)
+
+    rid = None
+    for line in res:
+        if line[1] == 'new-descr':
+            rid = line[4]
+    
+    r.delete_code_revision(rid)
+
+    res = r.get_dates_by_code_id3(code_id)
+    assert(len(res) == 1)
+
+def test_050_update_dates():
+    r = _test_get_conn()
+    r.create_db()
+    r.create_first_code()
+
+    res = r.get_codes_by_like_code('%')
+    code_id = res[0][0]
+    res = r.get_dates_by_code_id3(code_id)
+    assert(len(res) == 1)
+    rid = res[0][4]
+
+    r.revise_code(rid, "new-descr", 'A')
+    res = r.get_dates_by_code_id3(code_id)
+    assert(len(res) == 2)
+
+    dates = [[x[4], '', x[2], '', x[3]] for x in res]
+    # format rid, date_from, date_from_days, date_to, date_to_days
+    dates[1][2] -= 1
+    new_data = dates[1][2]
+    r.update_dates(dates)
+
+    res = r.get_dates_by_code_id3(code_id)
+    assert(res[1][2] == new_data)
+
+def test_050_get_config():
+    r = _test_get_conn()
+    r.create_db()
+    r.create_first_code()
+    
+    res = r.get_config()
+    # check only that no exception is raised
+
+"""
+    read method
+            "search_revisions",
+            "get_children_by_rid",
+            "get_bom_dates_by_code_id",
+            "get_drawings_by_code_id",
+            "get_where_used_from_id_code",
+            "get_bom_by_code_id3",
+            "is_child",
+            "is_assembly",
+            "get_children_dates_range_by_rid",
+            "get_parent_dates_range_by_code_id",
+            "get_codes_by_like_code_and_descr",
+            "get_codes_by_like_descr",
+            "get_codes_by_like_code",
+            "get_code_by_rid",
+            "get_code",
+            "dump_tables",
+            "list_main_tables",
+            "dump_table",
+
+"""
+
 def main():
-    if sys.argv[1] == "--client-test":
+    if len(sys.argv) > 1 and sys.argv[1] == "--client-test":
+        """
         r = RemoteSQLClient()
         r.remote_server_do_auth("foo", "bar")
         print("r.remote_server_get_id:", r.remote_server_get_id())
@@ -260,8 +467,13 @@ def main():
         print("r.remote_server_get_id:", r2.remote_server_get_id())
         #print("get_codes_by_like_code:", r2.get_codes_by_like_code("%"))
         print("r.remote_server_get_id:", r2.remote_server_get_id())
+        """
+        last = 2
+        import test_db
+        test_db.run_test(sys.argv[last:], sys.modules[__name__],
+            print_exc=True)
 
-    elif sys.argv[1] == "--server":
+    elif len(sys.argv) > 1 and sys.argv[1] == "--server":
         import cfg
         cfg.init()
 
