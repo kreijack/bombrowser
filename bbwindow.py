5ddb3be262c5932db2c8c6ffebb9b639963fc067
Goffredo Baroncelli <kreijack@inwind.it>
2023-03-06 20:15:14 +0100

    register the shortcut key ctrl-l/w
    Register the shortcut key even when the menu is not showed.

--- bbwindow.py
+++ bbwindow.py
@@ -77,6 +77,8 @@ class BBMainWindow(QMainWindow):
             win = self
 
         wm = main_menu.addMenu(title)
+        # to install the shortcut key CTRL-L/CTRL-W
+        self._build_windows_menu(wm, win)
         wm.aboutToShow.connect(
             lambda : self._build_windows_menu(wm, win))
 
