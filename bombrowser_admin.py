e79453e3619e9b8a3023928cd5c5d184182f4dd7
Goffredo Baroncelli <kreijack@inwind.it>
2024-05-29 22:06:15 +0200

    Improved bombrowser_admin.py

--- bombrowser_admin.py
+++ bombrowser_admin.py
@@ -20,13 +20,14 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 import sys
 import zipfile
+import os
 from datetime import datetime
 
 from PySide2.QtWidgets import QPushButton, QMainWindow, QSpinBox, QLabel
 from PySide2.QtWidgets import QTextEdit, QFileDialog, QMessageBox
 from PySide2.QtWidgets import QGridLayout, QApplication, QGroupBox, QWidget
 
-import db, utils, cfg
+import db, utils, cfg, version
 
 class AdminWindow(QMainWindow):
     def __init__(self, parent=None):
@@ -35,7 +36,7 @@ class AdminWindow(QMainWindow):
         self._init_gui()
         self._refresh_columns_number()
 
-        self._log_append("Ready!\n")
+        self._log_append("Connection: %s\n"%(db.connection))
 
         self.setWindowTitle("BOMBrowser database admin")
 
@@ -55,42 +56,51 @@ class AdminWindow(QMainWindow):
 
         b =QPushButton("Change gval/gaval number of columns...")
         b.clicked.connect(self._change_columns)
-        g.addWidget(b, 30, 10)
+        g.addWidget(b, 30, 10, 1, 3)
+
+        g.setColumnStretch(10, 100)
 
         gb1 = QGroupBox("gval/gaval columns")
         gb1.setLayout(g)
 
         g = QGridLayout()
-        b =QPushButton("Backup database...")
+        b =QPushButton("Backup database")
         b.clicked.connect(self._backup_database)
         g.addWidget(b, 30, 10)
 
-        b =QPushButton("Restore database...")
+        b =QPushButton("Restore database")
         b.clicked.connect(self._restore_database)
         g.addWidget(b, 40, 10)
+
+        b =QPushButton("Automatic database backup")
+        b.clicked.connect(self._automatic_backup_2)
+        g.addWidget(b, 50, 10)
+
 
         gb2 = QGroupBox("Backup/Restore database")
         gb2.setLayout(g)
 
         g = QGridLayout()
-        b =QPushButton("Init database...")
+        b =QPushButton("Init database")
         b.clicked.connect(self._init_database)
         g.addWidget(b, 40, 10)
 
         gb3 = QGroupBox("Init database")
         gb3.setLayout(g)
 
+
         g = QGridLayout()
         b =QPushButton("Info database")
         b.clicked.connect(self._info_database)
         g.addWidget(b, 40, 10)
 
-        b =QPushButton("Info database export...")
+        b =QPushButton("Info exported database")
         b.clicked.connect(self._info_database_export)
         g.addWidget(b, 50, 10)
 
         gb4 = QGroupBox("Info database")
         gb4.setLayout(g)
+
 
         g = QGridLayout()
         g.addWidget(gb1, 10, 1, 1, 5)
@@ -125,13 +135,13 @@ class AdminWindow(QMainWindow):
             return
 
         with utils.OverrideCursor():
+            self._log_append("Init database:")
+            self._log_append("----------------------------")
 
-            backupfile="bombrowser-%s.bkp.zip"%(datetime.now().strftime("%Y%f%m%H%M%S"))
-            self._log_append("Make backup to %s"%(backupfile))
-            db.dump_tables(backupfile, db.get_db_instance(), quiet=True)
+            self._automatic_backup()
 
             db.new_db(db.get_db_instance())
-        self._log.append("The database was initialized\n")
+        self._log_append("The database was initialized\n")
 
     def _info_database(self):
         tables = db.get_db_instance().list_main_tables()
@@ -148,13 +158,15 @@ class AdminWindow(QMainWindow):
             c.execute("SELECT MIN(date_from) FROM item_revisions")
             min_date_from = c.fetchone()[0]
 
+        self._log_append("Info database:")
+        self._log_append("----------------------------")
         for i in tables:
-            self._log.append("Table %s: %d rows"%(i, stat[i]))
-        self._log.append("Last revision: %s"%(max_date_from))
-        self._log.append("First revision: %s"%(min_date_from))
-        self._log.append("gval columns count: %d"%(db.gvals_count))
-        self._log.append("gaval columns count: %d"%(db.gavals_count))
-        self._log.append("")
+            self._log_append("Table %s: %d rows"%(i, stat[i]))
+        self._log_append("Last revision: %s"%(max_date_from))
+        self._log_append("First revision: %s"%(min_date_from))
+        self._log_append("gval columns count: %d"%(db.gvals_count))
+        self._log_append("gaval columns count: %d"%(db.gavals_count))
+        self._log_append("")
 
     def _info_database_export(self):
         tables = db.get_db_instance().list_main_tables()
@@ -167,18 +179,27 @@ class AdminWindow(QMainWindow):
         nf = nf[0]
         stat = dict()
 
-        gval, gaval = self._count_gval_gaval(nf)
-        self._log.append("Export: %s"%(nf))
-        self._log.append("")
+
+        self._log_append("Info exported database:")
+        self._log_append("----------------------------")
+        self._log_append(self._get_info_exported_database(nf))
+
+    def _get_info_exported_database(self, nf):
+
+        ret = "File: %s\n"%(nf)
+
         with zipfile.ZipFile(nf) as z:
             for zfn in z.namelist():
                 cnt = 0;
                 for line in z.open(zfn).readlines():
                     cnt += 1
-                self._log_append("%s: %d rows"%(zfn, cnt-1))
-        self._log.append("gval columns count: %d"%(gval))
-        self._log.append("gaval columns count: %d"%(gaval))
-        self._log.append("")
+                ret += "File %s: %d rows\n"%(zfn, cnt-1)
+
+        gval, gaval = self._count_gval_gaval(nf)
+        ret += "gval columns count: %d\n"%(gval)
+        ret += "gaval columns count: %d\n"%(gaval)
+
+        return ret
 
     def _log_append(self, s):
         self._log.setText(self._log.toPlainText() + s + "\n")
@@ -225,48 +246,85 @@ class AdminWindow(QMainWindow):
             )
 
     def _change_columns(self):
+
+        if (db.gvals_count == self._gval_count.value() and
+            db.gavals_count == self._gaval_count.value()):
+                return
+
+        self._log_append("Change columns count:")
+        self._log_append("----------------------------")
+
+        self._automatic_backup()
+
         with db.Transaction(db.get_db_instance()) as t:
-            if db.gvals_count != self._gval_count.value():
-                reply = QMessageBox.question(self,"Confirm the change of column",
-                            "Found %d gvals columns, will be set to %d :\n"%(
-                                db.gvals_count, self._gval_count.value()) +
-                            "Do you want to continue ?",
-                                        QMessageBox.Yes, QMessageBox.No);
-                if reply != QMessageBox.Yes:
-                    self._log_append("Change number of gval aborted !!!\n")
-                else:
-                    if db.gvals_count > self._gval_count.value():
-                        for i in range(self._gval_count.value() + 1, db.gvals_count + 1):
-                            self._drop_column(t, "item_revisions", "gval%d"%(i))
-                    else: # drop columns
-                        for i in range(db.gvals_count + 1 , self._gval_count.value() + 1):
-                            self._add_column(t, "item_revisions", "gval%d"%(i))
-
-                    self._log_append("Change number of gval from %d to %d\n"%(
-                                    db.gvals_count, self._gval_count.value()))
-
-
-            if db.gavals_count != self._gaval_count.value():
-                reply = QMessageBox.question(self,"Confirm the change of column",
-                            "Found %d gavals columns, will be set to %d :\n"%(
-                                db.gavals_count, self._gaval_count.value()) +
-                            "Do you want to continue ?",
-                                        QMessageBox.Yes, QMessageBox.No);
-                if reply != QMessageBox.Yes:
-                    self._log_append("Change number of gaval aborted !!!\n")
-                else:
-                    if db.gavals_count > self._gaval_count.value():
-                        # drop columns
-                        for i in range(self._gaval_count.value() + 1, db.gavals_count + 1):
-                            self._drop_column(t, "assemblies", "gaval%d"%(i))
-                    else: # drop columns
-                        for i in range(db.gavals_count + 1 , self._gaval_count.value() + 1):
-                            self._add_column(t, "assemblies", "gaval%d"%(i))
-
-                    self._log_append("Change number of gaval from %d to %d\n"%(
-                                    db.gavals_count, self._gaval_count.value()))
+            self._change_gval_column(t)
+            self._change_gaval_column(t)
 
         self._refresh_columns_number()
+        self._log_append("")
+
+    def _change_gval_column(self, t):
+        if db.gvals_count == self._gval_count.value():
+            return
+
+        reply = QMessageBox.question(self,"Confirm the change of column",
+                    "Found %d gvals columns, will be set to %d :\n"%(
+                        db.gvals_count, self._gval_count.value()) +
+                    "Do you want to continue ?",
+                                QMessageBox.Yes, QMessageBox.No);
+        if reply != QMessageBox.Yes:
+            self._log_append("Change number of gval aborted !!!\n")
+            return
+
+        reply = QMessageBox.question(self,"Confirm the change of column",
+                    "Are you really convinced to change the column gval ?",
+                                QMessageBox.Yes, QMessageBox.No);
+        if reply != QMessageBox.Yes:
+            self._log_append("Change number of gval aborted !!!\n")
+            return
+
+        if db.gvals_count > self._gval_count.value():
+            for i in range(self._gval_count.value() + 1, db.gvals_count + 1):
+                self._drop_column(t, "item_revisions", "gval%d"%(i))
+        else: # drop columns
+            for i in range(db.gvals_count + 1 , self._gval_count.value() + 1):
+                self._add_column(t, "item_revisions", "gval%d"%(i))
+
+        self._log_append("Change number of gval from %d to %d"%(
+                        db.gvals_count, self._gval_count.value()))
+
+
+    def _change_gaval_column(self, t):
+        if db.gavals_count == self._gaval_count.value():
+            return
+
+        reply = QMessageBox.question(self,"Confirm the change of column",
+                    "Found %d gavals columns, will be set to %d :\n"%(
+                        db.gavals_count, self._gaval_count.value()) +
+                    "Do you want to continue ?",
+                                QMessageBox.Yes, QMessageBox.No);
+        if reply != QMessageBox.Yes:
+            self._log_append("Change number of gaval aborted !!!\n")
+            return
+
+        reply = QMessageBox.question(self,"Confirm the change of column",
+                    "Are you really convinced to change the column gaval ?",
+                                QMessageBox.Yes, QMessageBox.No);
+        if reply != QMessageBox.Yes:
+            self._log_append("Change number of gaval aborted !!!\n")
+            return
+
+        if db.gavals_count > self._gaval_count.value():
+            # drop columns
+            for i in range(self._gaval_count.value() + 1, db.gavals_count + 1):
+                self._drop_column(t, "assemblies", "gaval%d"%(i))
+        else: # drop columns
+            for i in range(db.gavals_count + 1 , self._gaval_count.value() + 1):
+                self._add_column(t, "assemblies", "gaval%d"%(i))
+
+        self._log_append("Change number of gaval from %d to %d"%(
+                        db.gavals_count, self._gaval_count.value()))
+
 
     def _backup_database(self):
         nf = QFileDialog.getSaveFileName(self, "Export backup",
@@ -278,6 +336,8 @@ class AdminWindow(QMainWindow):
         with utils.OverrideCursor():
             db.dump_tables(nf[0], db.get_db_instance(), quiet=True)
 
+        self._log_append("Backup database:")
+        self._log_append("----------------------------")
         self._log_append("Saved db to " + nf[0] + "\n")
 
     def _count_gval_gaval(self, nf):
@@ -306,10 +366,11 @@ class AdminWindow(QMainWindow):
         if nf[0] == "":
             return
 
-        gval, gaval = self._count_gval_gaval(nf[0])
-
+        info = self._get_info_exported_database(nf[0])
         reply = QMessageBox.question(self,"Confirm the restore",
-                    "Found %d gvals columns, %d gaval columns:\n"%(gval, gaval) +
+                    "-"*20 + "\n" +
+                    info +
+                    "-"*20 + "\n" +
                     "Do you want to continue to restore ?",
                                 QMessageBox.Yes, QMessageBox.No);
         if reply != QMessageBox.Yes:
@@ -322,12 +383,14 @@ class AdminWindow(QMainWindow):
             self._log_append("Restored db aborted !!!\n")
             return
         with utils.OverrideCursor():
+            gval, gaval = self._count_gval_gaval(nf[0])
             db.gvals_count = gval
             db.gavals_count = gaval
 
-            backupfile="bombrowser-%s.bkp.zip"%(datetime.now().strftime("%Y%f%m%H%M%S"))
-            self._log_append("Make backup to %s"%(backupfile))
-            db.dump_tables(backupfile, db.get_db_instance(), quiet=True)
+            self._log_append("Restore database:")
+            self._log_append("----------------------------")
+
+            self._automatic_backup()
 
             db.restore_tables(nf[0], db.get_db_instance(), quiet=True)
 
@@ -340,8 +403,21 @@ class AdminWindow(QMainWindow):
         self._gval_count.setValue(db.gvals_count)
         self._gaval_count.setValue(db.gavals_count)
 
+    def _automatic_backup_2(self):
+        self._log_append("Automatic backup database:")
+        self._log_append("----------------------------")
+        self._automatic_backup()
 
-def main():
+    def _automatic_backup(self):
+        if not os.path.exists("backup-db/"):
+            os.mkdir("backup-db/")
+        backupfile="backup-db/bombrowser-%s.bkp.zip"%(datetime.now().strftime("%Y%m%d%H%M%S"))
+        self._log_append("Start backup to %s ..."%(backupfile))
+        db.dump_tables(backupfile, db.get_db_instance(), quiet=True)
+        self._log_append("Backup ended")
+
+
+def bombrowser_init():
     app = QApplication(sys.argv)
 
     sys.excepthook = utils._show_exception
@@ -380,6 +456,11 @@ def main():
         utils.show_exception(msg="Cannot connect to database\nAbort\n")
         return
 
+    return app
+
+def main():
+
+    app = bombrowser_init()
 
     w = AdminWindow()
     w.show()
